1e12c0fb33ce1aeda77ebbe18d02ddc8e702a38e
Vishal-badona1 <68052615+Vishal-badona1@users.noreply.github.com>
2023-01-12 00:29:36 +0530

    Nodemon changed to node

--- server.js
+++ server.js
@@ -2,20 +2,30 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 dotenv.config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const routes = require('./routes/userRoutes')
 const userProfile = require('./routes/userProfile')
-const {  mongoose } = require('mongoose')
+const { mongoose } = require('mongoose')
 
 const app = express()
 app.use(express.json())
 const PORT = process.env.PORT || 5000
 mongoose.set('strictQuery', true);
 app.use(cors())
-app.listen(PORT,()=>console.log("connected to server"))
-app.use('/',routes)
-app.use('/user',userProfile)
+app.use('/', routes)
+app.use('/user', userProfile)
 
-const DB = process.env.DATABASE_URL
-mongoose.connect(DB).then(()=>{console.log("Connected With Db")}).catch(()=>console.log("Can't Connect With Db"))
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () =>
+            console.log(`server running on http://localhost:${PORT}`)
+        )
+    }
+    )
+    .catch((err) => console.log(err + " can't connect to db"));
 
 
